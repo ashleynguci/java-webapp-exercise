00e3e516f5ab7398d32f43cb391bf581a2b0841a
ashleynguci <trucanhftu2@gmail.com>
2019-04-24 10:50:00 +0300

    modify Ex_2_Greeting_Form.html

--- src/servlet_examples/GreetingServlet_Simple.java
+++ src/servlet_examples/GreetingServlet_Simple.java
@@ -12,40 +12,43 @@ import java.io.PrintWriter;
 /**
  * Servlet implementation class GreetingServlet
  * 
- * Note: This servlet is mapped to the URL pattern '/helloservlet'.
- * That is, the URL pattern (not the servlet class name) is to be used in 
- * when sending a request for this servlet!
+ * Note: This servlet is mapped to the URL pattern '/helloservlet'. That is, the
+ * URL pattern (not the servlet class name) is to be used in when sending a
+ * request for this servlet!
  */
 
 @WebServlet("/simpleGreetingServlet")
 public class GreetingServlet_Simple extends HttpServlet {
-	private static final long serialVersionUID = 1L;   // This is needed for removing a warning.
+	private static final long serialVersionUID = 1L; // This is needed for removing a warning.
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-		// Get a PrintWriter object for writing the HTML document to be sent to the browser
+		// Get a PrintWriter object for writing the HTML document to be sent to the
+		// browser
 		PrintWriter out = response.getWriter();
-		
+
 		// 1. Retrieve the values of the request parameters
+		String title = request.getParameter("txtTitle");
 		String firstName = request.getParameter("txtFirstName");
 		String lastName = request.getParameter("txtLastName");
-		
+
 		// 2. Compose the greeting text
-		String greetingText = "Hello, "	+ firstName + " " + lastName + "!";
-		
+		String greetingText = "Hello, " + title + " " + firstName + " " + lastName + "!";
+
 		// 3. Write the text to the HTML document
 		out.println("<!DOCTYPE html>");
 		out.println("<html><head>");
 		out.println("<title>Hello</title><meta charset='UTF-8'>");
 		out.println("</head><body>");
 		out.println("<p>");
-		
-		out.println(greetingText);	// Here we insert the greeting into the HTML document!
-		
+
+		out.println(greetingText); // Here we insert the greeting into the HTML document!
+
 		out.println("</p>");
 		out.println("<p><a href='Ex_2_Greeting_Form.html'>Try again</a></p>");
 		out.println("</body></html>");
 	}
-	
+
 }
 // End
